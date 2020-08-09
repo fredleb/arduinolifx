5f060e9fe84fa8f164cbfcfb6ed3664517ee925f
Fidji El Fripon <fidji@gonzales.fritz.box>
2020-08-09 12:40:53 +0200

    LifxPacket old struct: remove

--- src/LifxPacket.hpp
+++ src/LifxPacket.hpp
@@ -1,22 +1,5 @@
 #include <Arduino.h>
 
-#if 0
-struct LifxPacket_old {
-  uint16_t size; //little endian
-  uint16_t protocol; //little endian
-  uint32_t reserved1;
-  byte bulbAddress[6];
-  uint16_t reserved2;
-  byte site[6];
-  uint16_t reserved3;
-  uint64_t timestamp;
-  uint16_t packet_type; //little endian
-  uint16_t reserved4;
-  
-  byte data[128];
-  int data_size;
-};
-#endif
 
 /**
  * Lifx Frame structure
