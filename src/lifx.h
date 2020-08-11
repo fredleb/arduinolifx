8dc4fe27ae706f82f8c4ad634767ed1dca58b8ab
Fidji El Fripon <fidji@gonzales.fritz.box>
2020-08-11 16:01:14 +0200

    Lifx.h: message definitions are no longer used

--- src/lifx.h
+++ src/lifx.h
@@ -25,58 +25,6 @@ const unsigned int LifxBulbVersion = 1;
 const unsigned int LifxFirmwareVersionMajor = 1;
 const unsigned int LifxFirmwareVersionMinor = 5;
 
-const byte SERVICE_UDP = 0x01;
-const byte SERVICE_TCP = 0x02;
-
-// packet types
-const byte GET_SERVICE = 0x02;
-const byte STATE_SERVICE = 0x03;
-
-const byte GET_HOST_INFO = 0x0C;
-const byte STATE_HOST_INFO = 0x0D;
-
-const byte GET_WIFI_INFO = 0x10;
-const byte STATE_WIFI_INFO = 0x11;
-
-const byte GET_WIFI_FIRMWARE_STATE = 0x12;
-const byte WIFI_FIRMWARE_STATE = 0x13;
-
-const byte GET_POWER_STATE = 0x14;
-const byte SET_POWER_STATE = 0x15;
-const byte POWER_STATE = 0x16;
-
-const byte GET_BULB_LABEL = 0x17;
-const byte SET_BULB_LABEL = 0x18;
-const byte BULB_LABEL = 0x19;
-
-const byte GET_VERSION_STATE = 0x20;
-const byte VERSION_STATE = 0x21;
-
-const byte GET_BULB_TAGS = 0x1a;
-const byte SET_BULB_TAGS = 0x1b;
-const byte BULB_TAGS = 0x1c;
-
-const byte GET_BULB_TAG_LABELS = 0x1d;
-const byte SET_BULB_TAG_LABELS = 0x1e;
-const byte BULB_TAG_LABELS = 0x1f;
-
-const byte ACKNOWLEDGEMENT = 0x2D;
-
-const byte ECHO_REQUEST = 0x3A;
-const byte ECHO_RESPONSE = 0x3B;
-
-const byte GET_LIGHT_STATE = 0x65;
-const byte SET_LIGHT_STATE = 0x66;
-const byte LIGHT_STATUS = 0x6b;
-
-const byte GET_POWER_STATE2 = 0x74;
-const byte SET_POWER_STATE2 = 0x75;
-const byte POWER_STATE2 = 0x76;
-
-const byte GET_MESH_FIRMWARE_STATE = 0x0e;
-const byte MESH_FIRMWARE_STATE = 0x0f;
-
-
 #define EEPROM_BULB_LABEL_START 0 // 32 bytes long
 #define EEPROM_BULB_TAGS_START 32 // 8 bytes long
 #define EEPROM_BULB_TAG_LABELS_START 40 // 32 bytes long
@@ -91,22 +39,6 @@ struct LifxEEPROM {
   char sSSID[LIFX_WIFI_SSID_LENGTH + 1];
   char sPassword[LIFX_WIFI_PASSWORD_LENGTH + 1];
 };
-
-
-// message structures and their answers
-struct Lifx_GET_LIGHT_STATE__ANSWER {
-    uint16 hue;          // LE
-    uint16 saturation;   // LE
-    uint16 brightness;   // LE
-    uint16 kelvin;       // LE
-
-    short reserved0;
-
-    uint16 power;
-    char bulb_label[LIFX_LABEL_LENGTH]; // UTF-8 encoded string
-    uint64 tags;
-};
-
 
 #define SET_LIGHT_ORIGIN_SETUP 0
 #define SET_LIGHT_ORIGIN_SET_LIGHT_STATE 1
